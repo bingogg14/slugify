24915119a8f60e50c39539e511637f3a39d9bdcc
Pavlo Harashchenko <bingogg14@gmail.com>
2023-04-11 21:03:56 +0300

    fix default language by null

--- src/Slugify.php
+++ src/Slugify.php
@@ -40,7 +40,7 @@ class Slugify implements SlugifyInterface
         'remove' => null,      // remove characters that match regex, defaults to `undefined`
         'lower' => false,      // convert to lower case, defaults to `false`
         'strict' => false,     // strip special characters except replacement, defaults to `false`
-        'locale' => 'vi',      // language code of the locale to use
+        'locale' => null,      // language code of the locale to use
         'trim' => true         // trim leading and trailing replacement chars, defaults to `true`
     ];
 
